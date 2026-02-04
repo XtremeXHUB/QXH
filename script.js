318da43021723cc3f009a601511f36b7f9c8e4b4
XtremeXHUB <155776254+XtremeXHUB@users.noreply.github.com>
2026-02-04 08:14:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+// Copy Script
 function copyScript(id, btn) {
     const text = document.getElementById(id).innerText;
-    const original = btn.innerText;
+    const old = btn.innerText;
 
     navigator.clipboard.writeText(text).then(() => {
         btn.innerText = "Copied ✅";
-        btn.style.background = "#00cc99";
-
-        setTimeout(() => {
-            btn.innerText = original;
-            btn.style.background = "";
-        }, 1500);
+        setTimeout(() => btn.innerText = old, 1500);
     });
 }
+
+// Announcement slide
+let index = 0;
+const list = document.querySelectorAll(".announcement");
+
+if (list.length > 0) {
+    setInterval(() => {
+        list.forEach(a => a.classList.remove("active"));
+        index = (index + 1) % list.length;
+        list[index].classList.add("active");
+    }, 4000);
+}
+
+// Close Announcement
+function closeAnnouncement(e) {
+    e.stopPropagation();
+    document.querySelector(".announcement-box").style.display = "none";
+}
+
+// Auto Time
+document.querySelectorAll(".time").forEach(el => {
+    const t = new Date(el.dataset.time);
+    el.innerText = t.toLocaleString("th-TH");
+});
