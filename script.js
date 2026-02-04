fd46fa77b26d88d95bf573dec8b4a84b03049ad3
XtremeXHUB <155776254+XtremeXHUB@users.noreply.github.com>
2026-02-04 13:43:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,13 @@ function copyText(id) {
   alert("Copy แล้ว!");
 }
 
-function closeAnn() {
-  document.getElementById("announcement").style.display = "none";
-}
-
-function openTab(id, el) {
+function openTab(tabId, btn) {
   document.querySelectorAll('.tab-content')
     .forEach(t => t.classList.remove('active'));
 
   document.querySelectorAll('.tab')
     .forEach(b => b.classList.remove('active'));
 
-  document.getElementById(id).classList.add('active');
-  el.classList.add('active');
+  document.getElementById(tabId).classList.add('active');
+  btn.classList.add('active');
 }
