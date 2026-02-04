dd00192576983295e6d983c5e95cf232dc086f02
XtremeXHUB <155776254+XtremeXHUB@users.noreply.github.com>
2026-02-04 13:14:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ function copyText(id) {
 function closeAnn() {
   document.getElementById("announcement").style.display = "none";
 }
+
+function openTab(id, el) {
+  document.querySelectorAll('.tab-content')
+    .forEach(t => t.classList.remove('active'));
+
+  document.querySelectorAll('.tab')
+    .forEach(b => b.classList.remove('active'));
+
+  document.getElementById(id).classList.add('active');
+  el.classList.add('active');
+}
